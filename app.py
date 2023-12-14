9f7df33e58ccdb2e156556c44d371be55b672c7c
Dmitrii Efremov <kvizad@gmail.com>
2023-12-14 14:42:38 +0200

    update of app.py and html

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from datetime import datetime
-from pytz import timezone
-import os
+import pytz
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+time_zones = {
+    'Los_Angeles': 'America/Los_Angeles',
+    'Ufa': 'Europe/Ufa',
+    'Sofia': 'Europe/Sofia'
+}
+
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get-current-time', methods=['GET'])
+def get_current_time():
+    times = {}
+    for city, tz in time_zones.items():
+        local_time = datetime.now(pytz.timezone(tz)).strftime('%H:%M')
+        times[city] = local_time
+    return jsonify(times)
 
 @app.route('/get-time', methods=['POST'])
 def get_time():
@@ -16,24 +29,17 @@ def get_time():
         city = data['city']
         time_input = data['time']
 
-        time_zones = {
-            'Los_Angeles': 'America/Los_Angeles',
-            'Ufa': 'Europe/Ufa',
-            'Sofia': 'Europe/Sofia'
-        }
-
         city_time_zone = time_zones.get(city.replace(' ', '_'))
         if not city_time_zone:
             raise ValueError("Invalid city name")
 
         naive_time = datetime.strptime(time_input, '%H:%M')
-        local_time = timezone(city_time_zone).localize(naive_time)
+        local_time = pytz.timezone(city_time_zone).localize(naive_time)
 
-        times = {city: local_time.astimezone(timezone(tz)).strftime('%H:%M') for city, tz in time_zones.items()}
+        times = {city: local_time.astimezone(pytz.timezone(tz)).strftime('%H:%M') for city, tz in time_zones.items()}
         return jsonify(times)
     except Exception as e:
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
